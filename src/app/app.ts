7e3c13364c65174bee16dabaaba81dd5de1e5a60
Taras Halaburda <tarashalaburda@gmail.com>
2026-06-18 16:47:59 +0300

    Section 2: Why would we want to use signals? - 4. section introduction: why would we want to use signals? & 5. Angular Default change detection and OnPush

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('modern-angular-21-with-signals');
+  counter = 0;
+
+  constructor(private cdr: ChangeDetectorRef) {
+    setInterval(() => {
+      this.counter++;
+      this.cdr.detectChanges();
+    }, 3000)
+  }
 }
