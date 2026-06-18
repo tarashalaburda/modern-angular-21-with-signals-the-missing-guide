6914a2fabae9477b7fbff2cfd0811559d1622f28
Taras Halaburda <tarashalaburda@gmail.com>
2026-06-18 18:01:53 +0300

    6. Invoking change detection when in the OnPush & added import for zone.js for test

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   counter = 0;
@@ -12,7 +13,15 @@ export class App {
   constructor(private cdr: ChangeDetectorRef) {
     setInterval(() => {
       this.counter++;
+      console.log('setInterval counter', this.counter);
+    }, 1000);
+
+    setInterval(() => {
+
       this.cdr.detectChanges();
-    }, 3000)
+    }, 5000)
+  }
+
+  doNothing(): void {
   }
 }
