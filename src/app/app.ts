7f03434672b984387e25b670e6f261487e61e73d
Taras Halaburda <tarashalaburda@gmail.com>
2026-06-24 11:51:59 +0300

    13. RxJS State management challanges - Part 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { BehaviorSubject, combineLatest, debounceTime, map } from 'rxjs';
+import { BehaviorSubject, combineLatest, debounceTime, firstValueFrom, map } from 'rxjs';
 
 type Options = Record<string, string>;
 
@@ -11,13 +11,30 @@ type Options = Record<string, string>;
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
+  readonly a$ = new BehaviorSubject<number>(1);
+  readonly b$ = new BehaviorSubject<number>(2);
+
+  readonly sum$ = combineLatest([this.a$, this.b$]).pipe(map(([a, b]) => a + b));
+
+  async incrementA() {
+    // only increment A if A + B is less than 10
+
+    const sum = await firstValueFrom(this.sum$);
+
+    console.log('sum -', sum);
+
+    if (sum < 10) {
+      this.a$.next(this.a$.value + 1);
+    }
+  }
+
   readonly option$ = new BehaviorSubject<Options>({ r: 'Red', g: 'Green', b: 'Blue' });
 
   readonly selectedKey$ = new BehaviorSubject<string>('b');
 
   readonly selectedValue$ = combineLatest([this.option$, this.selectedKey$]).pipe(
     debounceTime(0),
-    map(([options, key]) => options[key])
+    map(([options, key]) => options[key]),
   );
 
   switchOptions() {
