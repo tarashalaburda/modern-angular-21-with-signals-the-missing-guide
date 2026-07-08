52a688f194c5e9a32a4d13e66ae3da6da85d65fd
Taras Halaburda <tarashalaburda@gmail.com>
2026-07-08 16:39:14 +0300

    34. Signals and Immutability

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
-import { ApiService } from './api.service';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { switchMap } from 'rxjs';
-
+import { Component, effect, signal } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
@@ -10,38 +6,23 @@ import { switchMap } from 'rxjs';
   styleUrl: './app.scss',
 })
 export class App {
-  readonly api = inject(ApiService);
+  readonly names = signal(['Alice', 'Bob', 'Charlie']);
 
-  readonly number = signal(10);
-
-  readonly number$ = toObservable(this.number);
-
-  readonly results$ = this.number$.pipe(
-    switchMap((number) => this.api.getPrimeFactors(number))
-  );
-
-  readonly primeFactors = toSignal(this.results$, {
-    initialValue: [],
-  });
-
-  increase() {
-    this.number.update((n) => n + 1);
-  }
-
-  decrease() {
-    this.number.update((n) => Math.max(n - 1, 3));
-  }
+  readonly person = signal({ name: 'Joni', age: 32 });
 
   constructor() {
-    this.number$.subscribe((n) => {
-      console.log('Observable subscription 1 ', n);
-    });
-
     setTimeout(() => {
-      console.log('Creating 2nd Subscription ');
-      this.number$.subscribe((n) => {
-        console.log('Observable subscription 2 ', n);
-      });
-    }, 5000);
+      this.names().push('David');
+      // this.names.update((names) => [...names, 'David']);
+
+      this.person.update(person => ({...person, name: 'Din'}))
+
+      console.log('Adding David to the list of names ', this.names());
+    }, 2000)
+
+    effect(() => {
+      console.log('the names are ', this.names());
+      console.log('person change  ', this.person());
+    });
   }
 }
