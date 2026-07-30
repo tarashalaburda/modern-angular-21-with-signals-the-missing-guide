b8f1d3beaa3edde773aac3ca0e42922879eaf727
Taras Halaburda <tarashalaburda@gmail.com>
2026-07-30 15:48:45 +0300

    48. Binding router data to component inputs - new approach

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners,
-  provideZonelessChangeDetection
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 
@@ -9,6 +11,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes, withComponentInputBinding()),
+  ],
 };
--- src/app/pages/movie-details/movie-details.ts
+++ src/app/pages/movie-details/movie-details.ts
@@ -1,24 +1,29 @@
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
+import { Component, computed, inject, input, numberAttribute } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie-details',
-  imports: [AsyncPipe],
+  imports: [],
   templateUrl: './movie-details.html',
   styleUrl: './movie-details.scss',
 })
 export class MovieDetails {
-  readonly route = inject(ActivatedRoute);
+  // readonly route = inject(ActivatedRoute);
   readonly moviesService = inject(MoviesService);
 
-  readonly id$ = this.route.params.pipe(map((params) => Number(params['id'])));
+  readonly id = input.required({ transform: numberAttribute });
 
-  readonly movie$ = this.id$.pipe(
-    map((id) => this.moviesService.movies().find((movie) => movie.id === id!)),
-  );
+  // readonly id$ = this.route.params.pipe(map((params) => Number(params['id'])));
 
-  readonly poster$ = this.movie$.pipe(map((movie) => `movies/${movie?.posterImage}`));
+  // readonly movie$ = this.id$.pipe(
+  //   map((id) => this.moviesService.movies().find((movie) => movie.id === id!)),
+  // );
+
+  readonly movie = computed(() =>
+    this.moviesService.movies().find((mv) => mv.id === this.id()));
+
+  // readonly poster$ = this.movie$.pipe(map((movie) => `movies/${movie?.posterImage}`));
+
+  readonly poster = computed(
+    () => `movies/${this.movie()?.posterImage ?? ''}`);
 }
