7c3dcf7d1e67d92ab1a6ccd5114b610a806d4210
Taras Halaburda <tarashalaburda@gmail.com>
2026-08-03 15:24:22 +0300

    58. Triggering APIs using RxJS Interop

--- src/app/services/exam.service.ts
+++ src/app/services/exam.service.ts
@@ -1,11 +1,16 @@
-import { computed, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { Question } from '../models/question.model';
 import { Answer } from '../models/answer.model';
+import { BehaviorSubject, switchMap, tap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ExamGeneratorService } from './exam-generator.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ExamService {
+  readonly #generateExam$ = new BehaviorSubject<number>(1);
+
   readonly #questions = signal<Question[]>([
     {
       caption: 'How much is 4 * 3',
@@ -27,6 +32,7 @@ export class ExamService {
   readonly #isBusy = signal<boolean>(false);
 
   readonly questions = this.#questions.asReadonly();
+  readonly level = toSignal(this.#generateExam$);
 
   readonly userAnswers = computed(() =>
     this.#userAnswers().map<Answer>((ans, index) => ({
@@ -45,5 +51,33 @@ export class ExamService {
 
   answerCurrentQuestion(answerIndex: number): void {
     this.#userAnswers.update((answers) => [...answers, answerIndex]);
+  }
+
+  increaseLevel(): void {
+    this.#generateExam$.next(this.#generateExam$.value + 1);
+  }
+
+  decreaseLevel(): void {
+    this.#generateExam$.next(this.#generateExam$.value - 1);
+  }
+
+  repeatLevel(): void {
+    this.#generateExam$.next(this.#generateExam$.value);
+  }
+
+  constructor() {
+    const generator = inject(ExamGeneratorService);
+
+    this.#generateExam$
+      .pipe(
+        tap(() => this.#isBusy.set(true)),
+        switchMap((level) => generator.generateExam(level)),
+        tap((questions) => {
+          this.#questions.set(questions);
+          this.#userAnswers.set([]);
+          this.#isBusy.set(false);
+        }),
+      )
+      .subscribe();
   }
 }
