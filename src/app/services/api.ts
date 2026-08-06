ff3e403c6f3c8498bbd57e6d6294dd9abe53bb63
Taras Halaburda <tarashalaburda@gmail.com>
2026-08-06 11:47:54 +0300

    68. Error Handling in Resources

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -29,13 +29,20 @@ export class Api {
     // this function returns the value times 5 after a delay of 3 seconds
 
     console.log('[API] Getting a multiplier for seed', value);
-    return new Promise<number>((resolve) => {
+    return new Promise<number>((resolve, reject) => {
       let handle: number | null = null;
 
       handle = setTimeout(() => {
         const res = value * 5;
-        console.log('[API] Multiplier received', res);
-        resolve(res);
+
+        if (value === 57) {
+          reject(' ⛔ I dont like number 57 ⛔');
+        } else {
+          console.log('[API] Multiplier received', res);
+          resolve(res);
+        }
+
+        handle = null;
       }, 3000);
 
       abortSignal?.addEventListener('abort', () => {
@@ -43,7 +50,7 @@ export class Api {
           clearTimeout(handle);
           console.log('mutiplyByFiveAsync - Canceled!!! ❌ ');
         }
-      })
+      });
     });
   }
 }
