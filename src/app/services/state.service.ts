d44ae3168ec2fff6c21e63e0ed13f4729f072528
Taras Halaburda <tarashalaburda@gmail.com>
2026-08-07 14:36:03 +0300

    74. Using "resource" with "fetch" for HTTP requests

--- src/app/services/state.service.ts
+++ src/app/services/state.service.ts
@@ -1,20 +1,37 @@
-import { Injectable, Signal, signal } from '@angular/core';
+import { Injectable, resource, Signal, signal } from '@angular/core';
+import { Book } from '../models/book';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StateService {
-  readonly apiBase = 'http://localhost:3000/api/books/';
+  readonly apiBase = 'http://localhost:3000/api/books';
   readonly wsBase = 'ws://localhost:3000/ws';
 
   #keyword = signal<string>('the');
+
+  #searchResult = resource({
+    params: () => ({ keyword: this.#keyword() }),
+    loader: (options) => this.#searchKeyWordPromise(options.params.keyword, options.abortSignal),
+    defaultValue: [],
+  });
 
   get keyword(): Signal<string> {
     return this.#keyword.asReadonly();
   }
 
+  get searchResult() {
+    return this.#searchResult.asReadonly();
+  }
+
   setKeyword(value: string) {
     console.log('Keyword changes to value,', value);
     this.#keyword.set(value);
+  }
+
+  #searchKeyWordPromise(value: string, abortSignal?: AbortSignal): Promise<Book[]> {
+    return fetch(`${this.apiBase}/search?q=${value}`, { signal: abortSignal }).then((res) =>
+      res.json(),
+    );
   }
 }
